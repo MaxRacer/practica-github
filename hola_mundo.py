0493f966642d2f2db0263a65ad35e18b33134488
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-09-12 20:06:33 +0200

    Updated_hola_mundo.py
    En el primer código sólo había una pregunta (¿A ti también te gusta?) y según la respuesta salía un mensaje.
    Al segundo código añadí:
    Una segunda pregunta (var2), que sólo aparece si la primera respuesta fue "S".
    Y, por último, nuevos mensajes dependiendo de si el usuario quiere aprender programación o no.

--- hola_mundo.py
+++
@@ -1,9 +0,0 @@
-import time
-print("Hola, me gusta Python")
-time.sleep(1)
-var1=input("¿A ti también te gusta? S/N")
-if var1=="S":
-    print("A mí también")
-else:
-    print("Que mal gusto tienes")
-
