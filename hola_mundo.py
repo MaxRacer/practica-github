caaf7a022d7175cd45f1efd506f1dfacdee58676
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-09-12 19:38:33 +0200

    ¡Mi primer código en Python!
    Estes es mi primer código en Python con el cual empieza mi iniciación en este mundo de la programación. ¡Seguiré haciendo más código y aprendiendo más!

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+import time
+print("Hola, me gusta Python")
+time.sleep(1)
+var1=input("¿A ti también te gusta? S/N")
+if var1=="S":
+    print("A mí también")
+else:
+    print("Que mal gusto tienes")
