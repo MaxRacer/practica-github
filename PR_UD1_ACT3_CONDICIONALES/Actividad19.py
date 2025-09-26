6d56d8acb17207b77b44e53113146e921fb719ee
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-09-26 16:05:20 +0200

    python

--- PR_UD1_ACT3_CONDICIONALES/Actividad19.py
+++ PR_UD1_ACT3_CONDICIONALES/Actividad19.py
@@ -1,1 +1,17 @@
-print("hola munda")
+#  Programa que introduzca dos números y devuelva cuál de los dos es mayor, menor o son 
+# iguales
+num1=float(input("Introduce un número"))
+num2=float(input("Introduce otro número"))
+
+if num1 > num2:
+    print(f"El {num1} es el mayor y el {num2} es el menor")
+else:
+    if num1 < num2:
+       print(f"El {num2} es el mayor y el {num1} es el menor")
+    else:
+        print(f"El {num1} y el {num2} son iguales")
+    
+
+ 
+
+    
