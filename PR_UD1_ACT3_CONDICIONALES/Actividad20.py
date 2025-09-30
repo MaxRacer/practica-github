9eea1976c09c583957ad2c1c87fabf2291cb6829
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-09-30 10:20:35 +0200

    python

---
+++ PR_UD1_ACT3_CONDICIONALES/Actividad20.py
@@ -0,0 +1,14 @@
+#A partir del ejercicio anterior, forzar que el usuario solo pueda introducir por teclados 
+# números entre 0 y 10
+num1=float(input("Introduce un número entre el 1-10"))
+num2=float(input("Introduce otro número entre el 1-10"))
+if num1 and num2 > 10:
+    print("Uno o los dos números están fuera de los límites establecidos")
+
+if num1 > num2:
+    print(f"El {num1} es el mayor y el {num2} es el menor")
+else:
+    if num1 < num2:
+       print(f"El {num2} es el mayor y el {num1} es el menor")
+    else:
+        print(f"El {num1} y el {num2} son iguales")
---
+++ Pruebas/Pruebas_condicional.py
@@ -0,0 +1,8 @@
+var1=float(input("Introduce un número:"))
+var2=float(input("Introduce otro número:"))
+
+if var1==var2:
+    print("Son iguales")    
+
+else:
+    print("Son distntos")
