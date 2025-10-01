bb8d3fdbe6ccb4bd2d39fd082c4c658e30039350
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-10-01 09:29:36 +0200

    Update Actividad20.py

--- PR_UD1_ACT3_CONDICIONALES/Actividad20.py
+++ PR_UD1_ACT3_CONDICIONALES/Actividad20.py
@@ -3,12 +3,13 @@
 num1=float(input("Introduce un número entre el 1-10"))
 num2=float(input("Introduce otro número entre el 1-10"))
 
-if num1 > num2:
-    print(f"El {num1} es el mayor y el {num2} es el menor")
-elif num1 and num2 >10:
+if num1 > 10 or num1 < 0 or num2 > 10 or num2 <0:
     print("Uno o los dos números están fuera de los límites establecidos")
 else:
-    if num1 < num2:
-       print(f"El {num2} es el mayor y el {num1} es el menor")
+    if num1 > num2:
+        print(f"El {num1} es el mayor y el {num2} es el menor")
     else:
-        print(f"El {num1} y el {num2} son iguales")
+        if num1 < num2:
+            print(f"El {num2} es el mayor y el {num1} es el menor")
+        else:
+            print(f"El {num1} y el {num2} son iguales")
