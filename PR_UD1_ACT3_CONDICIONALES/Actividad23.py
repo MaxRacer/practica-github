55e0cf9980f8a72b8c8b605c395fa46b73ba0016
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-10-03 15:34:36 +0200

    Create Actividad23.py

---
+++ PR_UD1_ACT3_CONDICIONALES/Actividad23.py
@@ -0,0 +1,20 @@
+#Modifica el programa anterior para establecer si la nota es un excelente (8.5 a 10), un 
+# notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota 
+# introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos.
+
+nota=float(input("Introduce la nota que has sacado: "))
+
+if 8.5 <= nota <= 10:
+    print(f"Tu nota es {nota}, excelente")
+elif 7 <= nota < 8.5:
+    print(f"Tu nota es {nota}, notable")
+elif 6 <= nota < 7:
+    print(f"Tu nota es {nota}, bien")
+elif 5 <= nota < 6: 
+    print(f"Tu nota es {nota}, suficiente")
+elif 0 <= nota < 5:
+    print(f"Tu nota es {nota}, insuficiente")
+else:
+    print("La nota que has introducido no está entre 0 y 10")
+        
+
