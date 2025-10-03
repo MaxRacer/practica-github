0767151e13a8f6edd731c01e11862913d05e2138
MaxRacer <maximo.arjona@campus.xaloc.org>
2025-10-03 15:35:01 +0200

    python

--- PR_UD1_ACT3_CONDICIONALES/Actividad23.py
+++ PR_UD1_ACT3_CONDICIONALES/Actividad23.py
@@ -1,4 +1,4 @@
-#Modifica el programa anterior para establecer si la nota es un excelente (8.5 a 10), un 
+# Modifica el programa anterior para establecer si la nota es un excelente (8.5 a 10), un 
 # notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota 
 # introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos.
 
