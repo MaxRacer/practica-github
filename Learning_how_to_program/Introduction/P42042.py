dc1ae37aaf696fc64a66e5fbd9d5896e2207370f
MaxRacer <maximo.arjona@campus.xaloc.org>
2026-02-11 09:56:20 +0100

    python

---
+++ Learning_how_to_program/Introduction/P42042.py
@@ -0,0 +1,6 @@
+letra=input("")
+vocales=["aeiouAEIOU"]
+if letra in vocales:
+    if letra.isupper():
+        print("vowel")
+        print("uppercase")
---
+++ Learning_how_to_program/Introduction/P56559.py
@@ -0,0 +1,8 @@
+a1, b1, a2, b2=map(int, input().split())
+if a1==a2 and b1==b2:
+    print("=")
+elif a2 <= a1 and b1 <= b2:
+    print("1")
+elif a1 <= a2 and b2 <= b1:
+    print("2")
+else: print("?")
