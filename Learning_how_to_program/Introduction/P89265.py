62f404e2261db4ef0c4f814f2958b185503ac9a0
MaxRacer <maximo.arjona@campus.xaloc.org>
2026-02-17 10:30:50 +0100

    python

--- Learning_how_to_program/Introduction/P89265.py
+++ Learning_how_to_program/Introduction/P89265.py
@@ -5,4 +5,17 @@ b1=int(partes[1])
 a2=int(partes[2])
 b2=int(partes[3])
 if a1==a2 and b1==b2:
-    print("=")
+    resultado="="
+elif a1>=a2 and b1<=b2:
+    resultado="1"
+elif a2>=a1 and b2<=b1:
+    resultado="2"
+else:
+    resultado="?"
+izquierda=max(a1, a2)
+derecha=min(b1, b2)
+if izquierda>derecha:
+    intervalo="[]"
+else:
+    intervalo=f"[{izquierda},{derecha}]"
+print(f"{resultado } , {intervalo}")
