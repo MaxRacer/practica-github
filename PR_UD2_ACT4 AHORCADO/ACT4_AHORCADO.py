2e6485e999eaafa04759a23b2d090dc1cdc8815e
MaxRacer <maximo.arjona@campus.xaloc.org>
2026-02-20 16:06:47 +0100

    python

--- PR_UD2_ACT4 AHORCADO/ACT4_AHORCADO.py
+++ PR_UD2_ACT4 AHORCADO/ACT4_AHORCADO.py
@@ -1,10 +1,11 @@
 import random
-Lista_palabrasecreta=["alonso"]
+Lista_palabrasecreta=["alonso", "hamilton", "vertappen", "carlos", "colapinto", "perez", "bottas", "albon",
+"hadjar", "russel"                 
+]
 Lista_partida=[]
 Lista_ahorcado=[]
 palabra=random.choice(Lista_palabrasecreta)
 for i in range(0, len(palabra)):
     Lista_partida.append("_")
 print(*Lista_partida)
-
 print("")
