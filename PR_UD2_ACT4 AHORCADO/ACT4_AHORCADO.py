ad5f8694c5ddfcbcca78dd666ae999f7356f1c06
MaxRacer <maximo.arjona@campus.xaloc.org>
2026-02-27 16:02:46 +0100

    python

--- PR_UD2_ACT4 AHORCADO/ACT4_AHORCADO.py
+++ PR_UD2_ACT4 AHORCADO/ACT4_AHORCADO.py
@@ -3,15 +3,25 @@ Lista_palabrasecreta=["Alonso"]
 Lista_partida=[]
 Lista_ahorcado=["A", "H", "O", "R", "C", "A", "D", "O"]
 palabra=random.choice(Lista_palabrasecreta).upper()
+errores=0
 for i in range(len(palabra)):
     Lista_partida.append("_")
 print(*Lista_partida)
 print("")
+while "_" in Lista_partida and errores<8:
+    letra=input("Introduce una letra: ").upper()
+    if letra in palabra:
+        for x in range(len(palabra)):
+            if palabra[x]==letra:
+                Lista_partida[x]=letra
+    else:
+        errores=errores+1
+        print("La letra introducido no está en la palabra")
+        print(*Lista_ahorcado[:errores])
+    print(*Lista_partida)
+if "_" not in Lista_partida:
+    print("¡HAS GANADO!")
+elif errores==8:
+    print("HAS PERDIDO")
 
-while "_" in Lista_partida:
-        letra=input("Introduce una letra: ").upper()
-        if letra in palabra:
-            for x in range(len(palabra)):
-                if palabra[x] == letra:
-                    Lista_partida[x] = letra
-            print(*Lista_partida)
+
